1954772126c706dc5da74ddb5c404e42fe31e824
peterjakemanHO <Peter.Jakeman@digital.homeoffice.gov.uk>
2024-03-11 17:08:43 +0000

    change gemfile

--- Gemfile
+++ Gemfile
@@ -1,14 +1,15 @@
+# If you do not have OpenSSL installed, change
+# the following line to use 'http://'
 source 'https://rubygems.org'
 
 # For faster file watcher updates on Windows:
-gem 'wdm', '~> 0.1.0', platforms: [:mswin, :mingw]
+gem 'wdm', '~> 0.1.0', platforms: [:mswin, :mingw, :x64_mingw]
 
 # Windows does not come with time zone data
-gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
+gem 'tzinfo-data', platforms: [:mswin, :mingw, :x64_mingw, :jruby]
 
+# Haml file is limited to 5.x as 6.0 has problem with middleman
+gem "haml", "~> 5.0" 
+gem "middleman-core", "4.5.0"
+# Include the tech docs gem
 gem 'govuk_tech_docs'
-
-gem 'middleman-gh-pages'
-
-# Include linter to check for dead internal links
-gem 'html-proofer'
--- config.rb
+++ config.rb
@@ -1,19 +1,16 @@
 require 'govuk_tech_docs'
 
-# Check for broken links
-require 'html-proofer'
+GovukTechDocs.configure(self)
 
-GovukTechDocs.configure(self, livereload: { js_host: "localhost" })
-
-after_build do |builder|
-  begin
-    HTMLProofer.check_directory(config[:build_dir],
-      { :assume_extension => true,
-        :disable_external => true,
-        :allow_hash_href => true,
-        :empty_alt_ignore => true,
-        :url_swap => { config[:tech_docs][:host] => "" } }).run
-  rescue RuntimeError => e
-    abort e.to_s
-  end
+# Build-specific configuration
+configure :build do
+  set :build_dir, '../public'
+  activate :relative_assets
+  set :relative_links, true
 end
+
+set :markdown_engine, :kramdown
+set :markdown,
+    fenced_code_blocks: true,
+    tables: true,
+    no_intra_emphasis: true
